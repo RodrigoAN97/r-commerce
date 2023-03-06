62d46accb40c356f1fcbaffdec000f8fff1c50ee
RodrigoAN97 <rodrigo_neves97@outlook.com>
2023-03-06 20:00:21 -0300

    db

---
+++ server/Data/StoreContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace server.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
